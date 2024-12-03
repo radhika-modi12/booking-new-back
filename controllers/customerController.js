bf4c3e4884540fbce810e257940693749e656096
radhika-modi12 <radhika.h.modi11@gmail.com>
2024-12-03 14:29:16 +0530

    update custom query changes in customcontroller

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -13,7 +13,7 @@ exports.createCustomer = async (req, res) => {
     const typeCheckQuery = `
       SELECT * FROM customers 
       WHERE booking_date = ? 
-      AND (booking_type = 'full_day' OR booking_type = 'half_day')
+      AND (booking_type = 'full_day' OR booking_type = 'half_day' OR booking_type = 'custom')
     `;
   
     db.query(typeCheckQuery, [booking_date], async (err, results) => {
@@ -26,14 +26,51 @@ exports.createCustomer = async (req, res) => {
           .status(400)
           .send('Full Day or Half Day booking already exists; Full Day is not allowed.');
       }
+      if (booking_type === 'custom') {
+        const customQuery = `
+        SELECT * FROM customers 
+        WHERE booking_date = ? 
+        AND (booking_type = 'full_day' 
+         OR (booking_type = 'half_day' AND booking_slot = 'first_half') 
+           OR (booking_type = 'custom' AND booking_time BETWEEN '06:00:00' AND '12:00:00'));
+      `;
+      db.query(customQuery, [booking_date], async (err, results) => {
+        if (err) {
+          return res.status(500).send('Database query error.');
+        }
+    
+        if (results.length > 0) {
+          return res
+            .status(400)
+            .send('conflict detected.');
+        }
+        const insertCustomeQuery = `
+        INSERT INTO customers 
+        (customer_name, customer_email, booking_date, booking_type, booking_slot, booking_time) 
+        VALUES (?, ?, ?, ?, ?, ?)
+      `;
+
+      db.query(
+        insertCustomeQuery,
+        [customer_name, customer_email, booking_date, booking_type, booking_slot, booking_time],
+        (err, results) => {
+          if (err) {
+            return res.status(500).json({ error: err.message });
+          }
+          return res.status(201).json({ message: 'Customer created successfully.' });
+        }
+      );
+      })
+      }
   
       // If booking type is Half Day, check for conflicts in specific slots
-      if (booking_type === 'half_day') {
+    else if (booking_type === 'half_day') {
         const halfDayQuery = `
           SELECT * FROM customers 
           WHERE booking_date = ? 
           AND (booking_type = 'full_day' OR 
-              (booking_type = 'half_day' AND booking_slot = ?))
+              (booking_type = 'half_day' AND booking_slot = ?)
+              OR (booking_type = 'custom' AND booking_time BETWEEN '06:00:00' AND '12:00:00'));
         `;
   
         db.query(halfDayQuery, [booking_date, booking_slot], async (err, halfDayResults) => {
